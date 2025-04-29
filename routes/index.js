a88d5a4d32d6c2ea372549ef4936ec76688e3836
leonardoalta <leonardoalta7688@gmail.com>
2025-04-29 09:39:34 -0600

    cambios fronted

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = Router();
 
 router.post('/foods', createFood);
 router.get('/foods', getAllFoods);
-
+router.put('/foods/:id', updateFood);
+router.delete('/foods/:id', deleteFood);
 module.exports = router;
